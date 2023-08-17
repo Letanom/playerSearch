f5ccf75294d03a66a772ebf97082000b0573c901
Letanom <kevinozsimsek@gmail.com>
2023-08-17 10:14:14 +0300

    updatev7

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,59 @@
-import { useState } from 'react'
-import data from './data.json'
-import './App.css'
+import React, { useState } from 'react';
+import data from './data.json';
+import './App.css';
 
 function App() {
-  
-  const [searchItem, setItem] = useState("")
+  const [searchItem, setSearchItem] = useState('');
 
   const handleChange = (event) => {
-    setItem(event.target.value)
-  }
-
-  const handleFocus = () => { }
+    setSearchItem(event.target.value);
+  };
 
   return (
     <div className='container'>
       <h1 className='head'>Fenerbahce Players</h1>
-      <span className='info'>Search with Player Name , Position and Jersey Number </span>
+      <span className='info'>
+        Search with Player Name, Position, and Jersey Number
+      </span>
       <div className='inputContainer'>
         <input
           className='search-bar'
           type='text'
           onChange={handleChange}
           placeholder=' '
-
-    
         />
       </div>
 
       <div className='dataContainer'>
-        {
-          data.filter((val) => {
-
-            if (searchItem == "") {
-              return val;//Boşsa search datanın içindeki tüm değerleri döndür
-
-            }
-            else if (searchItem == val.name) {
+        {data
+          .filter((val) => {
+            if (searchItem === '') {
               return val;
-
-            }
-            else if (searchItem == val['Forma Numarası']) {
+            } else if (searchItem === val.name) {
+              return val;
+            } else if (searchItem === val['Forma Numarası']) {
+              return val;
+            } else if (searchItem === val.Pozisyon) {
               return val;
             }
-            else if (searchItem == val.Pozisyon) {
-              return val;
-            }
-          }).map((val) => {
-            return (
-              <div className="data" key={val.id}>
-                <img className='img' src={val.Img} ></img>
-                <h3>{val.name}</h3>
-                <h4>{val.Pozisyon}</h4>
-                <h5>{val['Forma Numarası']}</h5>
-
-              </div>
-            )
+            return null;
           })
+          .map((val) => (
+            <div className='data' key={val.id}>
+              {/* Resimleri Web URL'leriyle Yükleme */}
+              <img className='img' src={val.Img} alt={val.name} />
 
-        }
+              {/* Resimleri Yerel Olarak Yükleme */}
+              {/* <img className='img' src={process.env.PUBLIC_URL + val.Img} alt={val.name} /> */}
+              
+              <h3>{val.name}</h3>
+              <h4>{val.Pozisyon}</h4>
+              <h5>{val['Forma Numarası']}</h5>
+            </div>
+          ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
